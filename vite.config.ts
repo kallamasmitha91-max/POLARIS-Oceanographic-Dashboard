5fb636ebb2b872107f99c6c53b8bd013cb839206
kallamasmitha91-max <kallamasmitha91@gmail.com>
2026-09-07 10:31:36 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/POLARIS-Oceanographic-Dashboard/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
